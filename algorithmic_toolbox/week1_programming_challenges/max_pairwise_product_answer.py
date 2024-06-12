8c2247f3abe517ffb0b21c916507d9ab3631d3fe
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-06-13 00:55:17 +0900

    uopdate

--- algorithmic_toolbox/week1_programming_challenges/max_pairwise_product_answer.py
+++ algorithmic_toolbox/week1_programming_challenges/max_pairwise_product_answer.py
@@ -1,12 +1,34 @@
 def max_pairwise_product(numbers):
-    n = len(numbers)
-    max_product = 0
-    for first in range(n):
-        for second in range(first + 1, n):
-            max_product = max(max_product,
-                numbers[first] * numbers[second])
+    numbers.sort()
+    neg = 0
+    for num in numbers:
+        if num < 0:
+            neg += 1
 
-    return max_product
+    maxi = float('-inf')
+    if neg >= 2:
+        maxi = max((numbers[0] * numbers[1]), maxi)
+
+    return max((numbers[-1] * numbers[-2]), maxi)
+
+
+def max_pairwise_product_n(numbers: list[int]) -> int:
+    """
+    this implementation is big O(n) comapred to the nlogn solution above. This is done by passing by the list to find
+    the two largest numbers. the text suggests doing thing by doing two passed over the list but it can be done in a
+    single pass
+    :param numbers:
+    :return:
+    """
+    first = float('-inf')
+    second = float('-inf')
+
+    for num in numbers:
+        if num > first:
+            second = first
+            first = num
+    return first * second
+
 
 
 if __name__ == '__main__':
--- algorithmic_toolbox/week1_programming_challenges/max_pairwise_product_test.py
+++ algorithmic_toolbox/week1_programming_challenges/max_pairwise_product_test.py
@@ -1,11 +1,39 @@
 import unittest
-from max_pairwise_product_answer import max_pairwise_product
+from max_pairwise_product_answer import max_pairwise_product, max_pairwise_product_n
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        tests = [[[10, 9], 90],
+                 [[10, 9, 8], 90],
+                 [[1,2,3,4], 12]]
+        for arg, ans in tests:
+            self.assertEqual(max_pairwise_product(arg), ans)  # add assertion here
 
+    def test_lengthly_input(self):
+        arg = [x for x in range(10000000)]
+        ans = 99999970000002
+        self.assertEqual(max_pairwise_product(arg), ans)
+
+    def test_negative_input(self):
+        arg = [-1, -2, -3, -4]
+        ans = 12
+        self.assertEqual(max_pairwise_product(arg), ans)
+
+    def test_negative_input_2(self):
+        arg = [-1, 2, 3, 4]
+        ans = 12
+        self.assertEqual(max_pairwise_product(arg), ans)
+
+    def test_negative_input_3(self):
+        arg = [-1, -2, 3, 4]
+        ans = 12
+        self.assertEqual(max_pairwise_product(arg), ans)
+
+    def test_n_time_function(self):
+        arg = [x for x in range(10000000)]
+        ans = 99999970000002
+        self.assertEqual(max_pairwise_product_n(arg), ans)
 
 if __name__ == '__main__':
     unittest.main()
