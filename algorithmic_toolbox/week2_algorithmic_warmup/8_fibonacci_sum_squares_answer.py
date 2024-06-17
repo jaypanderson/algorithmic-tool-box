2f67576d8874e6c082ea2774a3888b1c512dce39
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-06-17 23:11:09 +0900

    new function

---
+++ algorithmic_toolbox/week2_algorithmic_warmup/8_fibonacci_sum_squares_answer.py
@@ -0,0 +1,31 @@
+def fibonacci_sum_squares(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current = 1
+    period = 0
+
+    for _ in range(100):
+        previous, current = current, (previous + current)
+        period += 1
+        if previous % 10 == 0 and current % 10 == 1:
+            break
+
+    n %= period
+
+    if n <= 1:
+        return n
+
+    previous, current, _sum = 0, 1, 1
+
+    for _ in range(n - 1):
+        previous, current = current, (previous + current) % 10
+        _sum += (current ** 2) % 10
+
+    return _sum % 10
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print(fibonacci_sum_squares(n))
