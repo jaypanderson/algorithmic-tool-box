3110bb30d6ccddf21643b7af23f92b86e5710128
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-06-22 02:52:22 +0900

    bug fix

--- algorithmic_toolbox/week3_greedy_algorithms/6_maximum_number_of_prizes_answer.py
+++ algorithmic_toolbox/week3_greedy_algorithms/6_maximum_number_of_prizes_answer.py
@@ -1,10 +1,15 @@
 def optimal_summands(n):
     summands = [1]
     total = 1
+    remain = n - 1
     while total < n:
         new = summands[-1] + 1
+        if remain < new:
+            summands[-1] += remain
+            break
         summands.append(new)
         total += new
+        remain -= new
         #print('total: ', total)
 
     return summands
