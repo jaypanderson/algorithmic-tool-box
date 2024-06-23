fe45ca0c083e16973348837f9b18e7767fb13373
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-06-24 00:00:22 +0900

    new weeks problems

--- algorithmic_toolbox/week3_greedy_algorithms/7_maximum_salary_answer.py
+++ algorithmic_toolbox/week3_greedy_algorithms/7_maximum_salary_answer.py
@@ -1,3 +1,13 @@
+'''
+just as a reminder the following syntax can be used when you want to access the two values when comparing for a sort as
+well as make use of additional variables for the sort.
+
+numbers.sort(key=cmp_to_key(lambda x, y: compare(x, y, z)))
+
+this way you can access the two values directly and then pass in as many variables into your custom compare function.
+'''
+
+
 from functools import cmp_to_key
 
 
