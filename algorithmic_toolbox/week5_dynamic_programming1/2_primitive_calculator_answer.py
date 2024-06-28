3f27dc8b29086af61da4c5abe553cfa94e234efe
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-06-29 02:59:39 +0900

    bug fix

--- algorithmic_toolbox/week5_dynamic_programming1/2_primitive_calculator_answer.py
+++ algorithmic_toolbox/week5_dynamic_programming1/2_primitive_calculator_answer.py
@@ -1,36 +1,26 @@
 def compute_operations(n):
-    dp = [n]
+    dp = [0] * (n + 1)
+    ops = [[], []]
     operators = [['1', '-'], ['2', '/'], ['3', '/']]
-    while n >= 2:
-        if n % 3 == 0:
-            n //= 3
-        elif n % 3 == 1:
-            n -= 1
-        elif n % 2 == 0:
-            n //= 2
-        else:
-             n -= 1
-        dp.append(n)
-    return dp[::-1]
+    for i in range(2, n + 1):
+        mini = float('inf')
+        cur_op = None
+        for num, op in operators:
+            temp = eval(str(i) + op + num)
+            if temp >= 0 and temp == int(temp) and dp[int(temp)] < mini:
+                mini = dp[int(temp)]
+                cur_op = [num, op]
+        ops.append(cur_op)
+        dp[i] = mini + 1
 
+    cur = n
+    ans = [n]
+    while ops[cur]:
+        num, op = ops[cur]
+        cur = int(eval(str(cur) + op + num))
+        ans.append(cur)
 
-
-
-
-
-
-
-
-
-    # for i in range(2, len(dp)):
-    #     mini = float('inf')
-    #     for num, op in operators:
-    #         temp = eval(str(i) + op + num)
-    #         if temp >= 0 and temp == int(temp):
-    #             mini = min(mini, dp[int(temp) - 1])
-    #     dp[i] = mini + 1
-
-    # return dp
+    return ans[::-1]
 
 # [[0, 1], [,]]
 
@@ -40,3 +30,17 @@ if __name__ == '__main__':
     output_sequence = compute_operations(input_n)
     print(len(output_sequence) - 1)
     print(*output_sequence)
+
+
+    # dp = [n]
+    # while n >= 2:
+    #     if n % 3 == 0:
+    #         n //= 3
+    #     elif n % 3 == 1:
+    #         n -= 1
+    #     elif n % 2 == 0:
+    #         n //= 2
+    #     else:
+    #          n -= 1
+    #     dp.append(n)
+    # return dp[::-1]
