8b6719a41f30fe697f736fc478bd06773c240710
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-06-29 20:24:10 +0900

    Update 1_maximum_amount_of_gold_answer.py

--- algorithmic_toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold_answer.py
+++ algorithmic_toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold_answer.py
@@ -2,8 +2,16 @@ from sys import stdin
 
 
 def maximum_gold(capacity, weights):
-    assert False
+    n = len(weights)
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
 
+    for i in range(1, n + 1):
+        for j in range(1, capacity + 1):
+            dp[i][j] = dp[i-1][j]
+            if weights[i-1] <= j:
+                dp[i][j] = max(dp[i][j], dp[i-1][j - weights[i-1]] + weights[i-1])
+
+    return dp[n][capacity]
 
 if __name__ == '__main__':
     input_capacity, n, *input_weights = list(map(int, stdin.read().split()))
